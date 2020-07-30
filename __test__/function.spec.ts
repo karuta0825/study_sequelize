ba6ff30fbc0da79949d1989c63944aaa87d9e231
karuta0825 <magune3g@gmail.com>
2020-07-30 20:53:29 +0900

    ci

---
+++ __test__/function.spec.ts
@@ -0,0 +1,49 @@
+import * as assert from 'power-assert';
+import { test } from '../src/func';
+
+// describe("func", () => {
+//   it('h関数呼び出し', () => {
+//     assert(h() === 'h');
+//   });
+
+//   it('g関数呼び出し', () => {
+//     assert(func.g() === 'g');
+//   });
+// });
+
+// describe("class", () => {
+//   it("Person", () => {
+//     const p = new Person("adam");
+//     assert(p instanceof Person);
+//   });
+
+//   it("call", () => {
+//     const p = new Person("adam");
+//     assert(p.callName() === "adam");
+//   });
+// });
+
+const table = [
+  [1, 1, 2, 'case1'],
+  [1, 2, 3, 'case2'],
+  [2, {}, 3, 'case3'],
+];
+
+it.each(table)('.add(%i, %i)', (a: number, b: number, expected, casename) => {
+  console.log(casename);
+  assert.equal(a + b, expected);
+});
+
+for (const [a, b, e, n] of table as any) {
+  it(n, () => {
+    assert.equal(a + b, e);
+  });
+}
+
+it('test', () => {
+  // console.log(process.env);
+  // console.log(process.env.MYDB);
+  // console.log(process.env.MYSAMPLE);
+
+  assert(test() === 2);
+});
