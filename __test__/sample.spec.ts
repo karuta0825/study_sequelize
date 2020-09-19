d7291955a9121dd01ef07521e16e56905b04fa06
karuta0825 <magune3g@gmail.com>
2020-09-20 01:24:28 +0900

    test

--- __test__/sample.spec.ts
+++ __test__/sample.spec.ts
@@ -9,81 +9,81 @@ it('test', () => {
   assert.deepEqual('adam', { d: 'adam' });
 });
 
-// describe('Project Model', () => {
-//   beforeEach(async () => {
-//     await db.Project.sync({ force: true });
-//     await db.Task.sync({ force: true });
-//   });
+describe('Project Model', () => {
+  beforeEach(async () => {
+    await db.Project.sync({ force: true });
+    await db.Task.sync({ force: true });
+  });
 
-//   afterAll((done) => {
-//     done();
-//   });
+  afterAll((done) => {
+    done();
+  });
 
-//   describe('create', () => {
-//     it('insert作成', async () => {
-//       const title = 'my awesome project';
-//       const description = 'woot woot. this will make me a rich man';
+  describe('create', () => {
+    it('insert作成', async () => {
+      const title = 'my awesome project';
+      const description = 'woot woot. this will make me a rich man';
 
-//       const project = db.Project.build({
-//         title,
-//         description,
-//       });
+      const project = db.Project.build({
+        title,
+        description,
+      });
 
-//       await project.save();
-//       const rawProjects = await db.Project.findAll({
-//         attributes: ['id', 'title', 'description'],
-//       });
-//       const [actual]: any = rawProjects.map((d) => d.toJSON());
+      await project.save();
+      const rawProjects = await db.Project.findAll({
+        attributes: ['id', 'title', 'description'],
+      });
+      const [actual]: any = rawProjects.map((d) => d.toJSON());
 
-//       const expect = {
-//         id: 1,
-//         title,
-//         description,
-//       };
-//       assert.deepEqual(actual, expect);
-//     });
-//   });
+      const expect = {
+        id: 1,
+        title,
+        description,
+      };
+      assert.deepEqual(actual, expect);
+    });
+  });
 
-//   describe('hooks', () => {
-//     beforeAll(() => {
-//       db.Project.addHook<any, any>('afterCreate', (project, options) => {
-//         return db.Task.create({
-//           projectId: project.id,
-//           title: 'hooks',
-//           description: 'hooks',
-//           deadline: new Date(),
-//         });
-//       });
-//     });
+  describe('hooks', () => {
+    beforeAll(() => {
+      db.Project.addHook<any, any>('afterCreate', (project, options) => {
+        return db.Task.create({
+          projectId: project.id,
+          title: 'hooks',
+          description: 'hooks',
+          deadline: new Date(),
+        });
+      });
+    });
 
-//     afterAll((done) => {
-//       db.Project.removeHook('afterCreate', 'afterCreate');
-//       done();
-//     });
+    afterAll((done) => {
+      db.Project.removeHook('afterCreate', 'afterCreate');
+      done();
+    });
 
-//     it('use hooks', async () => {
-//       await sequelize.transaction((transaction) => {
-//         return db.Project.create(
-//           {
-//             title: 'project',
-//             description: 'project',
-//           },
-//           { transaction }
-//         );
-//       });
+    it('use hooks', async () => {
+      await sequelize.transaction((transaction) => {
+        return db.Project.create(
+          {
+            title: 'project',
+            description: 'project',
+          },
+          { transaction }
+        );
+      });
 
-//       const rawProjects = await db.Project.findAll({
-//         attributes: ['id', 'title', 'description'],
-//         include: [
-//           {
-//             attributes: ['id', 'title', 'description', 'deadline'],
-//             model: db.Task,
-//           },
-//         ],
-//       });
+      const rawProjects = await db.Project.findAll({
+        attributes: ['id', 'title', 'description'],
+        include: [
+          {
+            attributes: ['id', 'title', 'description', 'deadline'],
+            model: db.Task,
+          },
+        ],
+      });
 
-//       const [acutal] = rawProjects.map((d) => d.toJSON());
-//       assert.deepEqual(acutal, {});
-//     });
-//   });
-// });
+      const [acutal] = rawProjects.map((d) => d.toJSON());
+      assert.deepEqual(acutal, {});
+    });
+  });
+});
